a9a42ccceb6a510f06773a9da6729274cd960fe0
Adam <afranklint16@gmail.com>
2015-06-19 16:34:47 -0700

    [Fix] Refactored listener code to improve wand usage
    Previously, this would not cancel the event if the player was not
    clicking an container block. This would cause the item to be used
    normally if clicking on regular blocks, which would sometimes have
    undesirable effects. The listener code has been rewritten to always
    cancel the event, if the player is holding the wand. Then it checks for
    a container block, and fills it if needed. This code also is smaller.

--- src/main/java/com/github/pocketkid2/fill/listeners/FillListener.java
+++ src/main/java/com/github/pocketkid2/fill/listeners/FillListener.java
@@ -1,14 +1,12 @@
 package com.github.pocketkid2.fill.listeners;
 
 import org.bukkit.Sound;
-import org.bukkit.block.Block;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.InventoryHolder;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import com.github.pocketkid2.fill.FillPlugin;
 import com.github.pocketkid2.fill.utils.Messages;
@@ -23,36 +21,29 @@ public class FillListener implements Listener {
 
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event) {
-		// Check for block interaction
-		if (event.hasBlock() && event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-			// Check for item
-			if (event.hasItem()) {
-				// Create first object
-				ItemStack stack = event.getItem();
+		// Check for item and wand first
+		if (event.hasItem() && event.getItem().hasItemMeta()) {
+			ItemMeta meta = event.getItem().getItemMeta();
+			if (meta.hasDisplayName() && meta.getDisplayName().equals(plugin.getWandName())) {
+				// Immediately cancel
+				event.setCancelled(true);
+				event.setUseInteractedBlock(Event.Result.DENY);
+				event.setUseItemInHand(Event.Result.DENY);
 
-				// Check for wand name
-				if (stack.getItemMeta().hasDisplayName() && stack.getItemMeta().getDisplayName().equals(plugin.getWandName())) {
-					// Create second object
-					Block block = event.getClickedBlock();
+				// Then check for a block
+				if (event.hasBlock() && event.getClickedBlock().getState() instanceof InventoryHolder) {
+					// Pass in the event's items. We must clone the held item to
+					// prevent the original item from losing it's wand name when
+					// it is removed for the purpose of filling the inventory
+					// holder.
+					plugin.fill(event.getClickedBlock(), event.getItem().clone());
 
-					// Check for container block
-					if (block.getState() instanceof InventoryHolder) {
-						// Fill container (item stack must be cloned to prevent
-						// the one being held from changing)
-						plugin.fill(block, stack.clone());
-
-						// Cancel any actions and events
-						event.setUseInteractedBlock(Event.Result.DENY);
-						event.setUseItemInHand(Event.Result.DENY);
-						event.setCancelled(true);
-
-						// Send message and/or sound if enabled
-						if (plugin.MESSAGE) {
-							event.getPlayer().sendMessage(Messages.FILLED);
-						}
-						if (plugin.SOUND) {
-							event.getPlayer().playSound(event.getClickedBlock().getLocation(), Sound.ITEM_PICKUP, 10, 1);
-						}
+					// Optionally activate message and sound
+					if (plugin.MESSAGE) {
+						event.getPlayer().sendMessage(Messages.FILLED);
+					}
+					if (plugin.SOUND) {
+						event.getPlayer().playSound(event.getClickedBlock().getLocation(), Sound.ITEM_PICKUP, 10, 1);
 					}
 				}
 			}
